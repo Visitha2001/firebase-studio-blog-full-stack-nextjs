f4b3b79c237b27a5dde576f77c6fea90ea8367d3
VisithaNR <ruwanmg99@gmail.com>
2025-06-20 04:26:03 +0000

    use dark theme.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn(inter.variable)}>
+    <html lang="en" className={cn(inter.variable, 'dark')}>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
